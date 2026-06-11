ea7a948782b9ce3db675107c95e6d36dc3732926
Sani Buckingham <sbuckingham@raiz.us>
2026-06-11 14:03:37 -0600

    init

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from src.Postgres import Postgres
 
-TABLE_DDL = [
-    "CREATE TABLE IF NOT EXISTS data (acctnbr NUMERIC, memberagreenbr NUMERIC, rtxnnbr NUMERIC, tranamt NUMERIC, rtxntypcd TEXT, postdate DATE)"
-]
+TABLE_DDL = "CREATE TABLE IF NOT EXISTS data (acctnbr NUMERIC, memberagreenbr NUMERIC, rtxnnbr NUMERIC, tranamt NUMERIC, rtxntypcd TEXT, postdate DATE)"
 
 
 postgres = Postgres()
